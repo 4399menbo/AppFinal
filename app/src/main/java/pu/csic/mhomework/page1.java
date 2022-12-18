5d605726cfd8f4b0359406926c30b04f0e3cd316
410903830 <s1090383@gm.pu.edu.tw>
2022-12-19 01:37:11 +0800

    aaa

--- app/src/main/java/pu/csic/mhomework/page1.java
+++ app/src/main/java/pu/csic/mhomework/page1.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class page1 extends AppCompatActivity {
+
+    Switch ChangeMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
-
         Button nextPageBtn = (Button)findViewById(R.id.start);
+        Button next2PageBtn = (Button)findViewById(R.id.admin);
+
+
+        nextPageBtn.setVisibility(View.VISIBLE);
+        next2PageBtn.setVisibility(View.INVISIBLE);
+
+
+        ChangeMode=findViewById(R.id.switch1);
+
         nextPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,14 +37,39 @@ public class page1 extends AppCompatActivity {
             }
         });
 
-        Button next2PageBtn = (Button)findViewById(R.id.admin);
         next2PageBtn.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+
+
                 Intent intent = new Intent();
+                intent.setPackage(getPackageName());
                 intent.setClass(page1.this , page4.class);
+
                 startActivity(intent);
             }
         });
+
+        ChangeMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+              if(ChangeMode.isChecked()){
+                  nextPageBtn.setVisibility(View.INVISIBLE);
+                  next2PageBtn.setVisibility(View.VISIBLE);
+                  Log.d("test","1");
+              }
+              else {
+                  nextPageBtn.setVisibility(View.VISIBLE);
+                  next2PageBtn.setVisibility(View.INVISIBLE);
+
+                  Log.d("test","0");
+              }
+            }
+        });
+
+
+
     }
 }
