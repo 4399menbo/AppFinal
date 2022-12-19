6ced923affce4cf429c528385904b7bfe2eb5fd7
410903830 <s1090383@gm.pu.edu.tw>
2022-12-19 23:31:23 +0800

    aaa

---
+++ app/src/main/java/pu/csic/mhomework/ViewPagerItem.java
@@ -0,0 +1,12 @@
+package pu.csic.mhomework;
+
+public class ViewPagerItem {
+
+    int imageID;
+
+
+    public ViewPagerItem(int imageID) {
+        this.imageID = imageID;
+
+    }
+}
--- app/src/main/java/pu/csic/mhomework/page1.java
+++ app/src/main/java/pu/csic/mhomework/page1.java
@@ -1,6 +1,7 @@
 package pu.csic.mhomework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,9 +12,13 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class page1 extends AppCompatActivity {
 
     Switch ChangeMode;
+    ViewPager2 viewPager2;
+    ArrayList<ViewPagerItem> viewPagerItemArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,18 +64,50 @@ public class page1 extends AppCompatActivity {
               if(ChangeMode.isChecked()){
                   nextPageBtn.setVisibility(View.INVISIBLE);
                   next2PageBtn.setVisibility(View.VISIBLE);
-                  Toast.makeText(getApplicationContext(), "1",Toast.LENGTH_LONG).show();
+                  Toast.makeText(getApplicationContext(), "索取firebase",Toast.LENGTH_LONG).show();
                   Log.d("test","1");
               }
               else {
                   nextPageBtn.setVisibility(View.VISIBLE);
                   next2PageBtn.setVisibility(View.INVISIBLE);
 
-                  Toast.makeText(getApplicationContext(), "0",Toast.LENGTH_LONG).show();
+                  Toast.makeText(getApplicationContext(), "索取arrayList",Toast.LENGTH_LONG).show();
                   Log.d("test","0");
               }
             }
         });
+
+
+
+        /////viewpager
+
+        viewPager2 = findViewById(R.id.viewpager);
+        int[] images = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d};
+
+
+
+        viewPagerItemArrayList = new ArrayList<>();
+
+        for (int i =0; i< images.length ; i++){
+
+            ViewPagerItem viewPagerItem = new ViewPagerItem(images[i]);
+            viewPagerItemArrayList.add(viewPagerItem);
+
+        }
+
+        VPAdapter vpAdapter = new VPAdapter(viewPagerItemArrayList);
+
+        viewPager2.setAdapter(vpAdapter);
+
+        viewPager2.setClipToPadding(false);
+
+        viewPager2.setClipChildren(false);
+
+        viewPager2.setOffscreenPageLimit(2);
+
+        viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+
 
 
 
